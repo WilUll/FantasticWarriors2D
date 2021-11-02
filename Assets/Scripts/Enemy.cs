63c4383f102a599030825d030432cfeab9e7b0c6
Sappels <sappels66@gmail.com>
2021-11-02 17:20:01 +0100

    Push before merging main into branch

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    
-    [SerializeField] private GameObject player;
-    [SerializeField] float enemySpeed = 4f;
-    
-    //public Transform target;
+
+    public GameObject player;
+   
+    public float enemySpeed = 2f;
+    public float enemyChaseSpeed = 4f;
+
+    public bool inRangeOfPlayer = false;
 
 
     void Start()
@@ -20,8 +22,14 @@ public class Enemy : MonoBehaviour
     void Update()
     {
 
+        if (inRangeOfPlayer)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, enemyChaseSpeed * Time.deltaTime);
+        }
+        else
+        {
 
-        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, enemySpeed * Time.deltaTime);
+        }
 
     }
 
@@ -29,8 +37,7 @@ public class Enemy : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            //set player as target
-            //alternatively call function for moving towards player if target is already set
+            inRangeOfPlayer = true;
         }
     }
 }
